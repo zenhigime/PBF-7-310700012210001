b1dbc01445200b673bc956610e07a85af9ec46b1
zenhigime <zenhigime@gmail.com>
2025-05-14 13:19:54 +0800

    Update vite.config.js
    add base:"/githubRepo/"

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/PBF-7-310700012210001/",
   plugins: [react()],
   server: {
     host: '0.0.0.0',
